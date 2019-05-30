604b4179973f95587f246634dc17af26899399a7
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 02:14:58 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  data = languages.values.reduce({}, :merge)
+
+  for style, group in languages do
+    for language in group.keys
+
+      if !data[language].include? :style
+        data[language][:style] = []
+      end
+
+      data[language][:style] << style
+    end
+  end
+
+  data
 end
